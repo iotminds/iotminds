95c17ca6f41e3e1603e346dfccad87919835a1bd
Burak Tahtacı <buraktahtaci@Burak-MacBook-Pro.local>
2017-10-02 17:40:11 +0300

    minor changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': '127.0.0.1',
         'user': 'root',
-        'password': 'lala'
+        'password': ''
     },
 	'database': 'iot2',
     'users_table': 'users'
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ var mysql = require("mysql")
 var db_config = {
 	host	: '127.0.0.1',
 	user	: 'root',
-	password: 'lala',
+	password: '',
 	database: 'iot2'
 }
 
