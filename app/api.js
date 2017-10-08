470d2e851b11f27bc4bc2bf10f6deb2aedf2f3b9
hexafluoride <xafluoride@gmail.com>
2017-10-08 20:30:02 +0300

    Backend changes

--- app/api.js
+++ app/api.js
@@ -4,66 +4,48 @@ var db = require("../config/db.js")
 var utilities = require("./utilities.js")
 
 router.get("/update",function (req,res) {
-	var key = req.query.key
-	var value = req.query.value
+    var key = req.query.key
+    var value = req.query.value
 
-	if(!key || !value)
-		return utilities.printError(res, "Invalid arguments")
+    if(!key || !value)
+        return utilities.printError(res, "Invalid arguments")
 
-	utilities.getKeyInfo(key, function(err, key_info) {
-		if(err)
-			return utilities.printError(res, err)
-
-		if(!utilities.checkKey(key_info, utilities.accessConstants.COMPONENT_WRITE))
-			return utilities.printError(res, "Insufficient privileges")
-
-		update_result = utilities.updateComponent(key_info.component_id, key_info.channel_id, value)
-
-		if(update_result instanceof Error)
-            return utilities.printError(res, update_result)
-
-		utilities.printSuccess(res)
-	})
-})
-
-router.get("/channels",function (req,res) {
-	var key = req.query.key
-
-	if(!key)
-		return utilities.printError(res, "Invalid arguments")
-
-	utilities.getKeyInfo(key, function(err, key_info)
-	{
+    utilities.executeWithKey(key, utilities.accessConstants.COMPONENT_WRITE, function(err, key_info)
+    {
         if(err)
             return utilities.printError(res, err)
 
-        if(!utilities.checkKey(key_info, utilities.accessConstants.CHANNEL_LIST))
-            return utilities.printError(res, "Insufficient privileges")
+        update_result = utilities.updateComponent(key_info.component_id, key_info.channel_id, value)
+
+        if(update_result instanceof Error)
+            return utilities.printError(res, update_result)
+
+        utilities.printSuccess(res)
+    })
+})
+
+router.get("/channels",function (req,res) {
+    utilities.executeWithKey(req.query.key, utilities.accessConstants.CHANNEL_LIST, function(err, key_info)
+    {
+        if(err)
+            return utilities.printError(res, err)
 
         utilities.getChannels(key_info.user_id, function(err, result)
-		{
+        {
             if(err)
                 return utilities.printError(res, err)
 
             utilities.printSuccess(res, result)
         })
-	})
+    })
 })
 
 router.get("/components", function(req, res)
 {
-    var key = req.query.key
-
-    if(!key)
-        return utilities.printError(res, "Invalid arguments")
-
-    utilities.getKeyInfo(key, function(err, key_info)
+    utilities.executeWithKey(req.query.key, utilities.accessConstants.COMPONENT_LIST, function(err, key_info)
     {
         if(err)
             return utilities.printError(res, err)
-
-        if(!utilities.checkKey(key_info, utilities.accessConstants.COMPONENT_LIST))
-            return utilities.printError(res, "Insufficient privileges")
 
         utilities.getComponents(key_info.channel_id, function(err, result)
         {
@@ -75,19 +57,12 @@ router.get("/components", function(req, res)
     })
 })
 
-router.get("/retrieve",function (req,res) {
-    var key = req.query.key
-
-    if(!key)
-        return utilities.printError(res, "Invalid arguments")
-
-    utilities.getKeyInfo(key, function(err, key_info)
+router.get("/retrieve",function (req,res)
+{
+    utilities.executeWithKey(req.query.key, utilities.accessConstants.COMPONENT_READ, function(err, key_info)
     {
         if(err)
             return utilities.printError(res, err)
-
-        if(!utilities.checkKey(key_info, utilities.accessConstants.COMPONENT_READ))
-            return utilities.printError(res, "Insufficient privileges")
 
         utilities.getComponentData(key_info.component_id, function(err, result)
         {
@@ -99,14 +74,59 @@ router.get("/retrieve",function (req,res) {
     })
 })
 
+router.get("/new_channel", function (req, res)
+{
+    var key = req.query.key
+
+    if(!key || !req.query.channel_name)
+        return utilities.printError(res, "Invalid arguments")
+
+    utilities.executeWithKey(key, utilities.accessConstants.CHANNEL_CREATE, function(err, key_info)
+    {
+        if(err)
+            return utilities.printError(res, err)
+
+        utilities.createChannel(
+            {
+                channel_name: req.query.channel_name,
+                channel_description: req.query.channel_description,
+                image_url: req.query.img_url,
+                owner_id: key_info.user_id
+            },
+            function(err, id)
+            {
+                if(err)
+                {
+                    return utilities.printError(res, err)
+                }
+
+                utilities.printSuccess(res, {channel_id: id})
+            }
+        )
+    })
+})
+
+router.get("/new_component", function (req, res) {
+
+})
+
+router.get("/delete_channel", function(req, res)
+{
+    var key = req.query.key
+
+    if(!key || !req.query.channel_id)
+        return utilities.printError(res, "Invalid arguments")
+
+})
+
 function isLoggedIn(req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
+    // if they aren't redirect them to the home page
+    res.redirect('/');
 }
 
 module.exports = router
--- app/utilities.js
+++ app/utilities.js
@@ -1,62 +1,116 @@
 var db = require("../config/db.js")
 
 exports.accessConstants =
-	{
-		COMPONENT_READ: 1,
-		COMPONENT_WRITE: 2,
-		COMPONENT_LIST: 4,
-		COMPONENT_CREATE: 8,
-		COMPONENT_REMOVE: 16,
-		CHANNEL_READ: 32,
-		CHANNEL_MODIFY: 64,
-		CHANNEL_LIST: 128,
-		CHANNEL_CREATE: 256,
-		CHANNEL_REMOVE: 512
-	}
+    {
+        COMPONENT_READ: 1,
+        COMPONENT_WRITE: 2,
+        COMPONENT_LIST: 4,
+        COMPONENT_CREATE: 8,
+        COMPONENT_REMOVE: 16,
+        CHANNEL_READ: 32,
+        CHANNEL_MODIFY: 64,
+        CHANNEL_LIST: 128,
+        CHANNEL_CREATE: 256,
+        CHANNEL_REMOVE: 512
+    }
 
-exports.getKeyInfo = function(key, callback) 
+exports.getKeyInfo = function(key, callback)
 {
-	db.query("SELECT * FROM api_keys WHERE api_key=?",[key],function (err, result) 
-	{
-		if(err)
-		{
-			callback(err, null)
-		}
-		else if(result == [])
-		{
-			callback(Error("INVALID_API_KEY"), null)
-		}
-		else {
-			if(!result[0])
-			{
-				callback(Error("INVALID_API_KEY"), null)
-				return
-			}
+    db.query("SELECT * FROM api_keys WHERE api_key=?",[key],function (err, result)
+    {
+        if(err)
+        {
+            callback(err, null)
+        }
+        else if(result == [])
+        {
+            callback(Error("INVALID_API_KEY"), null)
+        }
+        else {
+            if(!result[0])
+            {
+                callback(Error("INVALID_API_KEY"), null)
+                return
+            }
 
-			callback(null,
-				{
-					access: result[0].access,
-					user_id: result[0].user_id,
-					component_id: result[0].component_id,
-					channel_id: result[0].channel_id
-				})
-		}
-	})
+            callback(null,
+                {
+                    access: result[0].access,
+                    user_id: result[0].user_id,
+                    component_id: result[0].component_id,
+                    channel_id: result[0].channel_id
+                })
+        }
+    })
 }
 
-exports.updateComponent = function(component_id, channel_id, value) 
+exports.executeWithKey = function(key, privilege, callback)
 {
-	db.query("INSERT INTO data (value,component_id,channel_id,created_at) VALUES(?,?,?,NOW())", [value, component_id, channel_id], function (err,result)
-	{
-		if (err)
-		{
-			return err
-		}
-		else
-		{
-			return result
-		}
-	})
+    if(!key)
+        return callback(new Error("You need to provide an API key"))
+
+    if(!privilege)
+        return callback(new Error("Invalid privilege level"))
+
+    exports.getKeyInfo(key, function(err, key_info)
+    {
+        if(err)
+            return callback(err)
+
+        if(!exports.checkKey(key_info, privilege))
+            return callback(new Error("Insufficient privileges"))
+
+        callback(null, key_info)
+    })
+}
+
+exports.updateComponent = function(component_id, channel_id, value)
+{
+    db.query("INSERT INTO data (value,component_id,channel_id,created_at) VALUES(?,?,?,NOW())", [value, component_id, channel_id], function (err,result)
+    {
+        if (err)
+        {
+            return err
+        }
+        else
+        {
+            return result
+        }
+    })
+}
+
+exports.createChannel = function(channel_object, callback)
+{
+    db.query("INSERT INTO channels (owner_id,name,description,created_at,image_url) VALUES(?,?,?,NOW(),?)",
+        [
+            channel_object.owner_id,
+            channel_object.channel_name,
+            channel_object.channel_description + "",
+            channel_object.image_url + ""
+        ],
+        function (err, res)
+        {
+            callback(err, res.insertId)
+        }
+    )
+}
+
+exports.createComponent = function(component_object, callback)
+{
+    db.query("INSERT INTO components (channel_id, name, type, created_at, last_updated) VALUES (?, ?, ?, NOW(), NOW())",
+        [
+            component_object.channel_id,
+            component_object.name,
+            component_object.type
+        ],
+        function (err, res) {
+            callback(err, res.insertId)
+        })
+}
+
+exports.getChannel = function(channel_id, callback)
+{
+    db.query("SELECT * FROM channels WHERE channel_id = ?", channel_id, callback)
 }
 
 exports.getChannels = function(id, callback)
@@ -71,35 +125,35 @@ exports.getComponents = function(id, callback)
 
 exports.getComponentData = function(id, callback)
 {
-	db.query("SELECT id,value,created_at FROM data WHERE component_id = ?", id, callback)
+    db.query("SELECT id,value,created_at FROM data WHERE component_id = ?", id, callback)
 }
 
 exports.checkKey = function(key, access_type)
 {
-	if(!key || !key.access)
-		return false;
+    if(!key || !key.access)
+        return false;
 
-	return !!(key.access & access_type);
+    return !!(key.access & access_type);
 }
 
 exports.printError = function(res, msg, code)
 {
-	if(msg instanceof Error)
-		msg = msg.message
+    if(msg instanceof Error)
+        msg = msg.message
 
-	res.json(
-	{
-		result: "error",
-		error_code: code,
-		error_msg: msg
-	})
+    res.json(
+        {
+            result: "error",
+            error_code: code,
+            error_msg: msg
+        })
 }
 
 exports.printSuccess = function(res, val)
 {
-	res.json(
-	{
-		result: "success",
-		data: val
-	})
+    res.json(
+        {
+            result: "success",
+            data: val
+        })
 }
